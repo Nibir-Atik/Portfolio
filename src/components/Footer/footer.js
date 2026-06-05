3c662e2d969210c7c96ba64d1aa7419f8cb46bf9
Md Atik Hossain Nibir <mdatikhossainnibir@gmail.com>
2026-06-05 22:33:05 +0600

    Updated file

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <footer className='footer'>
-            Copyright &#169; 2023 Smith Williams. All right received.
-        </footer>
-    );
-}
+  return (
+    <footer className="footer">
+      Copyright &#169; 2026 MD Atik Hossain Nibir. All right received.
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
